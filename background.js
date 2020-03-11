c438b162c093d9683d2e887dd125c11aa9e732f3
Isaac Steiner <ilebwohlsteiner@gmail.com>
2020-03-11 08:27:18 -0700

    Added support for SD, IcM, OneCRM, and Global Search

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 // Set up context menu at install time.
 chrome.runtime.onInstalled.addListener(function() {
     var context = "selection";
-    var title = "Open SR or IcM";
+    var title = "Open SR %s"
     var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                           "id": "context" + context}); 
+                                           "id": "context" + context});
   });
   
   // add click event
@@ -11,7 +11,21 @@ chrome.runtime.onInstalled.addListener(function() {
   
   // The onClicked callback function.
   function onClickHandler(info, tab) {
-    var sText = info.selectionText;
-    var url = "https://servicedesk.microsoft.com/#/customer/cases?caseNumber=" + encodeURIComponent(sText);
-    window.open(url, '_blank');
+    var selectedText = info.selectionText.trim();
+    if(selectedText.length == 15) {
+        var url = "https://servicedesk.microsoft.com/#/customer/cases?caseNumber=" + encodeURIComponent(selectedText);
+        window.open(url, '_blank');
+    }
+     else if(selectedText.length == 9) {
+        var url = "https://portal.microsofticm.com/imp/v3/incidents/details/" + encodeURIComponent(selectedText);
+        window.open(url, '_blank');
+     }
+     else if(selectedText.length == 7) {
+         var url = "https://dynamicscrm.visualstudio.com/OneCRM/_workitems/edit/" + encodeURIComponent(selectedText);
+         window.open(url, '_blank');
+     }
+     else {
+         var url = "https://unify.services.dynamics.com/CRM/Collaborations/GlobalSearch?#search=" + encodeURIComponent(selectedText);
+         window.open(url, '_blank');
+     }
   };
