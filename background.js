0197c1299229f0db0ff1d5bd6dee17a9edc6ac2d
Isaac Steiner <ilebwohlsteiner@gmail.com>
2020-03-12 17:12:23 -0700

    Add images

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // Set up context menu at install time.
 chrome.runtime.onInstalled.addListener(function() {
     var context = "selection";
-    var title = "Open SR %s"
+    var title = "Open %s"
     var id = chrome.contextMenus.create({"title": title, "contexts":[context],
                                            "id": "context" + context});
   });
